edbbfc58a88fe9ed18714a9af5267d88d081989c
Puettse <biguno.media@gmail.com>
2026-01-07 13:14:43 -0600

    Add imports and function signatures in access.py

--- Feral_Kitty_FiFi/features/ safeword/access.py
+++ Feral_Kitty_FiFi/features/ safeword/access.py
@@ -1,1 +1,8 @@
+import discord
+from typing import Any, List
+from .constants import STAFF_FALLBACK_NAME
+from ..utils.discord_resolvers import normalize
+
+def member_authorized(member: discord.Member, roles_whitelist: List[Any]) -> bool: ...
+def member_blocked(member: discord.Member, blocked_roles: List[Any]) -> bool: ...
 
